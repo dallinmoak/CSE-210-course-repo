233034271eefac77e7d8b07ee4780f7618113b17
dallinmoak <dallinmoak@gmail.com>
2024-10-12 22:32:15 -0600

    finish journal code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,142 +1,236 @@
 using System;
 using System.Globalization;
+using System.IO;
 
 class Program
 {
-    class Entry
+  class Entry
+  {
+    public Entry(string promptText, string response, string date)
     {
-        public Entry(string promptText, string response, string date)
-        {
-            this._promptText = promptText;
-            this._response = response;
-            this._date = date;
-        }
-        public string _promptText;
-        public string _response;
-        public string _date;
+      this._promptText = promptText;
+      this._response = response;
+      this._date = date;
     }
+    public string _promptText;
+    public string _response;
+    public string _date;
+  }
 
-    class Prompt
+  class Prompt
+  {
+    public Prompt()
     {
-        public Prompt()
-        {
-            //TODO: actually randomize from a list of prompt
-            this._promptText = "What's on your mind?";
-        }
-        private string _promptText;
-        public Entry PerformPrompt()
-        {
-            Console.WriteLine(this._promptText);
-            string response = Console.ReadLine();
-            string date = DateTime.Now.ToString();
-            return new Entry(this._promptText, response, date);
-        }
+      List<string> possiblePrompts = new List<string> {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+      };
+      Random rand = new Random();
+      this._promptText = possiblePrompts[rand.Next(possiblePrompts.Count)];
     }
-
-    class Journal
+    private string _promptText;
+    public Entry PerformPrompt()
     {
-        public Journal(string filesource)
-        {
-            this._entries = Load(filesource);
-            this._formattedContents = FormatContents(this._entries);
+      Console.WriteLine(this._promptText);
+      string response = Console.ReadLine();
+      string date = DateTime.Now.ToString();
+      return new Entry(this._promptText, response, date);
+    }
+  }
 
-        }
-        private List<Entry> _entries;
-        private string _formattedContents;
-        public void DisplayContents()
+  class Journal
+  {
+    public Journal(string filesource = null)
+    {
+      if (filesource != null)
+      {
+        this.entries = Load(filesource);
+        this._formattedContents = FormatContents(this.entries);
+      }
+      else
+      {
+        this.entries = new List<Entry>();
+        this._formattedContents = "";
+      }
+    }
+    public List<Entry> entries;
+    private string _formattedContents;
+    public void DisplayContents()
+    {
+      Console.WriteLine(_formattedContents);
+    }
+    private static string FormatContents(List<Entry> entries)
+    {
+      string formattedContents = "Journal Entries:\n";
+      foreach (Entry entry in entries)
+      {
+        formattedContents += $"  Date: {entry._date.Substring(0, 10)}\n";
+        formattedContents += $"  Prompt: {entry._promptText}\n";
+        formattedContents += $"  {entry._response}\n";
+        formattedContents += "_______________________________\n";
+      }
+      return formattedContents;
+    }
+    private static List<Entry> Load(string filesource)
+    {
+      string errorMsg = "file load error. sorry for not being transparent about it, just try make sure the file exists and is formatted correctly.";
+      List<Entry> loadedEntries = new List<Entry>();
+      if (string.IsNullOrEmpty(filesource) || !File.Exists(filesource))
+      {
+        throw new Exception(errorMsg);
+      }
+      try
+      {
+
+        List<string> lines = System.IO.File.ReadAllLines(filesource).ToList();
+        for (int i = 0; i < lines.Count; i += 3)
         {
-            Console.WriteLine(_formattedContents);
+          loadedEntries.Add(new Entry(lines[i], lines[i + 1], lines[i + 2]));
         }
-        private static string FormatContents(List<Entry> entries)
+      }
+      catch (Exception e)
+      {
+        throw new Exception(errorMsg);
+      }
+      return loadedEntries;
+    }
+    public void PerformPrompt()
+    {
+      Entry entry = new Prompt().PerformPrompt();
+      Console.WriteLine($"adding entry: {entry._promptText} {entry._response} {entry._date}");
+      this.entries.Add(entry);
+      this._formattedContents = FormatContents(this.entries);
+    }
+    public void Save(string filesource)
+    {
+      try
+      {
+        using (StreamWriter sw = new StreamWriter(filesource))
         {
-            string formattedContents = "";
-            foreach (Entry entry in entries)
+          foreach (Entry entry in this.entries)
+          {
+            sw.WriteLine(entry._promptText);
+            sw.WriteLine(entry._response);
+            sw.WriteLine(entry._date);
+          }
+        }
+        Console.WriteLine($"journal has been saved as {filesource}.");
+      }
+      catch (Exception e)
+      {
+        throw new Exception("file save error.");
+      }
+    }
+  }
+
+  private static int ShowMenu()
+  {
+    while (true)
+    {
+      Console.WriteLine("*** Journal Menu ***");
+      Console.WriteLine("1. Write");
+      Console.WriteLine("2. Display");
+      Console.WriteLine("3. Load");
+      Console.WriteLine("4. Save");
+      Console.WriteLine("5. New journal");
+      Console.WriteLine("6. Quit");
+      Console.Write("What would you like to do? ");
+      string response = Console.ReadLine();
+      if (!int.TryParse(response, out int choice))
+      {
+        Console.WriteLine("Invalid input. Please enter a number.");
+      }
+      return choice;
+    }
+  }
+
+  static void Main(string[] args)
+  {
+    // Jacob (or B. Crosby), I'm generally not a big code commenter. I'm firmly in the well-written-code-doesn't-need-comments camp.
+    // BUT I noticed the rubric mentions comments for the "Shows creativty and exeeds core requirements" section, so please consider these comments an outline about how this project meets that requirement:
+    // 1. I've added a new feature to the journal program: the ability to create a new journal instance without loading an existing file. real world application is fairly ovbious.
+    // 2. I added some (minimal) error handling around saving and loading files, including try/catch blocks and thowing Exceptions.
+    // 3. The program gives the user feedback when they try to perform an action that isn't possible given the current state of the program. handled cases include:
+    //   - trying to write to a journal that hasn't been loaded or created yet
+    //   - trying to display entries when there are none or no jounrnal has been loaded
+    //   - trying to save a journal in either of the above situations.
+    // also included in my project directory are a couple sample journal files. feel free to use them during testing
+    Journal myJournal = null;
+    int choice = 0;
+    while (choice != 6)
+    {
+      Console.Write("\n");
+      choice = ShowMenu();
+      bool loadError = false;
+      bool choiceRequiresLoad = choice == 1 || choice == 2 || choice == 4;
+      if (myJournal == null && choiceRequiresLoad)
+      {
+        loadError = true;
+        Console.WriteLine("You must load a file or start a new journal first.");
+      }
+      bool choiceRequiresEntries = choice == 2 || choice == 4;
+      if (myJournal?.entries?.Count == 0 && choiceRequiresEntries)
+      {
+        loadError = true;
+        Console.WriteLine("You must have at least 1 entry first.");
+      }
+      if (!loadError)
+      {
+        switch (choice)
+        {
+          case 1: // write
+            Console.WriteLine("adding an entry to the current journal:");
+            myJournal.PerformPrompt();
+            break;
+
+          case 2: // display
+            myJournal.DisplayContents();
+            break;
+
+          case 3: // load
+            Console.Write("what file would you like to load? ");
+            string loadFileSource = Console.ReadLine();
+            try
             {
-                formattedContents += $"{entry._promptText} {entry._response} {entry._date}\n";
+              myJournal = new Journal(loadFileSource);
+              Console.WriteLine("loaded " + loadFileSource);
             }
-            return formattedContents;
-        }
-        private static List<Entry> Load(string filesource)
-        {
-            //TODO: actually load stuff from the file
-            return new List<Entry>{
-                new Entry("promptText", "response", "date"),
-                new Entry("promptText2", "response2", "date2")
-            };
-        }
-        public void PerformPrompt()
-        {
-            Entry entry = new Prompt().PerformPrompt();
-            Console.WriteLine($"adding entry: {entry._promptText} {entry._response} {entry._date}");
-            this._entries.Add(entry);
-            this._formattedContents = FormatContents(this._entries);
-        }
-        public void Save(string filesource)
-        {
-            // Save the contents to the file
-        }
-    }
+            catch (Exception e)
+            {
+              Console.WriteLine($"ERROR: {e.Message}");
+            }
+            break;
 
-    private static int ShowMenu()
-    {
-        while (true)
-        {
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
-            Console.WriteLine("5. Quit");
-            Console.WriteLine("What would you like to do?");
-            string response = Console.ReadLine();
-            if (!int.TryParse(response, out int choice))
+          case 4: // save
+            Console.Write("what file would you like to save to? ");
+            string saveFileSource = Console.ReadLine();
+            try
             {
-                Console.WriteLine("Invalid input. Please enter a number.");
+              myJournal.Save(saveFileSource);
             }
-            return choice;
-        }
-    }
+            catch (Exception e)
+            {
+              Console.WriteLine($"ERROR: {e.Message}");
+            }
+            break;
 
-    static void Main(string[] args)
-    {
-        Journal myJournal = null;
-        int choice = 0;
-        while (choice != 5)
-        {
-            choice = ShowMenu();
-            bool loadError = false;
-            bool choiceRequiresLoad = choice == 1 || choice == 2 || choice == 4;
-            if (myJournal == null && choiceRequiresLoad)
-            {
-                loadError = true;
-            }
-            if (!loadError)
-            {
-                if (choice == 1)
-                {
-                    myJournal.PerformPrompt();
-                }
-                else if (choice == 2)
-                {
-                    myJournal.DisplayContents();
-                }
-                else if (choice == 3)
-                {
-                    Console.Write("what file would you like to load?");
-                    string filesource = Console.ReadLine();
-                    myJournal = new Journal(filesource);
-                }
-                else if (choice == 4)
-                {
-                    Console.Write("what file would you like to save to?");
-                    string filesource = Console.ReadLine();
-                    myJournal.Save(filesource);
-                    Console.WriteLine("saved as " + filesource);
-                }
-                else if (choice == 5)
-                {
-                    Console.Write("Goodbye!");
-                }
-            }
+          case 5: // New journal
+            myJournal = new Journal();
+            Console.WriteLine("New journal created.");
+            break;
+
+          case 6: // quit
+            Console.Write("Goodbye!");
+            break;
+
+          default:
+            Console.WriteLine("Invalid choice, please select again.");
+            break;
         }
+      }
     }
+  }
 }
